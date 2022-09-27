1afb8d209e40e7a7b2d789b1c1bbd757d52cf840
SLeN808 <sidhardhlenka@gmail.com>
2022-09-27 14:00:28 +0530

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,37 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer_main">
+      <div className="footer_section_1">
+        <div className="footer_section_1_1">
+          <span className="material-symbols-outlined location_icon">
+            location_on
+          </span>
+          <div className="footer_sec1_txt">
+            <h2>Find Us</h2>
+            <p>Avenue Street, America.</p>
+          </div>
+        </div>
+        <div className="footer_section_1_1">
+          <span class="material-symbols-outlined call_icon">phone_in_talk</span>
+          <div className="footer_sec1_txt">
+            <h2>Dial</h2>
+            <p>+91 9989345599</p>
+          </div>
+        </div>
+        <div className="footer_section_1_1">
+          <span class="material-symbols-outlined mail_icon">mail</span>
+          <div className="footer_sec1_txt">
+            <h2>Mail Us</h2>
+            <p>devsid08@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <hr />
+    </div>
+  );
 }
 
 export default Footer;
